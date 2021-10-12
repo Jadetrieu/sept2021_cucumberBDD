081294cdf08299cda42d9d59e4fe40bfa723ac07
Jadetrieu <enamjade0921@icloud.com>
2021-10-12 13:26:32 -0500

    tuesday

---
+++ src/main/java/base/Practice.java
@@ -0,0 +1,21 @@
+package base;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Practice {
+
+
+     WebDriver driver=new ChromeDriver();
+   //  driver.get("http://qa.taltektc.com/home/login");
+
+
+  //     driver.findElement(By.name("email")).sendKeys("hasanyc2@gmail.com");
+   //    driver.findElement(By.name("password")).sendKeys("Test$1234");
+   //     driver.findElement(By.xpath("//input[@value='Log In']")).click();
+ //      try {
+  //         assertEquals(driver.findElement(By.xpath("//form[@id='profile_form']/legend")).getText(), "Welcome to TalentTek");
+  //     } catch (Error e) {
+  //        verificationErrors.append(e.toString());
+    //    }
+}
+}
---
+++ src/test/java/StepDEF/login_step.java
@@ -0,0 +1,27 @@
+package StepDEF;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class login_step {
+
+    @Given("I am at TalentTEK Homepage")
+    public void iAmAtTalentTEKHomepage() {
+        // selenium + java code goes here
+    }
+
+    @And("I enter a valid email address")
+    public void iEnterAValidEmailAddress() {
+
+    }
+
+    @And("I enter a valid password")
+    public void iEnterAValidPassword() {
+
+    }
+
+    @When("I click on Login Button")
+    public void iClickOnLoginButton() {
+    }
+}
